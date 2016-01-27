cc23bcf46bd9b6323d35eaba4b0ffe8b99cb3ebb
David Neumann <davidjneumann@gmail.com>
2016-01-27 18:30:56 -0500

    Create basic game. Initial commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	private int min;
+	private int max;
+	private int middle;
+	private int guesses;
+	private int maxGuesses = 6;
+	
+	public Text Instructions;
+	public Text MatchingGuess;
+	public Text GuessesLeft;
+	public Text Lower;
+	public Text Higher;
+	
+	// Use this for initialization
+	void Start () {
+		max = 1000;
+		min = 1;
+		guesses = -1;
+		
+		PrintInstructions();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			//print("Up Arrow was pressed");
+			GuessHigher();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)){
+			//print("Down Arrow was pressed");
+			GuessLower();
+		}
+	}
+	
+	void PrintInstructions(){
+		var guess = ((max + min) / 2);
+		
+		guesses += 1;
+		
+		if(guesses >= maxGuesses){
+			Application.LoadLevel("Win");
+		}
+		else if(maxGuesses - guesses == 1){
+			Instructions.text = "Is your number " + guess + "?";
+			MatchingGuess.text = "It is " + guess + "!";
+			GuessesLeft.text = "My guesses left: " + (maxGuesses - guesses);
+			Lower.text = "Nope!";
+			Higher.text = "";
+		}
+		else{
+			Instructions.text = "Is the number higher or lower than " + guess + "?";
+			Instructions.text += "\r\nUp = higher, Down = lower";
+			GuessesLeft.text = "My guesses left: " + (maxGuesses - guesses);
+			MatchingGuess.text = "It is " + guess + "!";
+		}
+	}
+	
+	public void GuessHigher(){
+		min = (max + min + 1) / 2;
+		PrintInstructions();
+	}
+	
+	public void GuessLower(){
+		max = (max + min) / 2;
+		PrintInstructions();		
+	}
+}
---
+++ Assets/scrLevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrLevelManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName)	{
+		Debug.Log("LoadLevel: trying to load " + levelName);
+		Application.LoadLevel(levelName);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+}
